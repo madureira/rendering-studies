ad538bf2cbb4dbb2f8cbe7a573fa61ef99febb9c
Rafael Madureira <rafael.madureira.santos@gmail.com>
2025-03-17 23:40:19 -0300

    Format file

--- src/app/cube/cube.cpp
+++ src/app/cube/cube.cpp
@@ -104,76 +104,35 @@ void Cube::update(Window *window)
 
 void Cube::createMesh()
 {
-    float vertices[] = {
+    float32 vertices[] = {
         // Positions          // Colors
-
         // Front face
-        -0.5f,
-        -0.5f,
-        0.5f,
-        1.0f,
-        0.0f,
-        0.0f, // Bottom-left
-        0.5f,
-        -0.5f,
-        0.5f,
-        0.0f,
-        1.0f,
-        0.0f, // Bottom-right
-        0.5f,
-        0.5f,
-        0.5f,
-        0.0f,
-        0.0f,
-        1.0f, // Top-right
-        -0.5f,
-        0.5f,
-        0.5f,
-        1.0f,
-        1.0f,
-        0.0f, // Top-left
+        -0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f,  // Bottom-left
+         0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 0.0f,  // Bottom-right
+         0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,  // Top-right
+        -0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.0f,  // Top-left
 
         // Back face
-        -0.5f,
-        -0.5f,
-        -0.5f,
-        1.0f,
-        0.0f,
-        1.0f,
-        0.5f,
-        -0.5f,
-        -0.5f,
-        0.0f,
-        1.0f,
-        1.0f,
-        0.5f,
-        0.5f,
-        -0.5f,
-        1.0f,
-        1.0f,
-        1.0f,
-        -0.5f,
-        0.5f,
-        -0.5f,
-        0.5f,
-        0.5f,
-        0.5f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.5f, 0.5f, 0.5f,
     };
 
-    // Indices for drawing the cube with GL_TRIANGLES
-    unsigned int indices[] = {
+    uint32 indices[] = {
         // Front face
-        0, 1, 2, 2, 3, 0,
+        0, 1, 2,  2, 3, 0,
         // Back face
-        4, 5, 6, 6, 7, 4,
+        4, 5, 6,  6, 7, 4,
         // Left face
-        4, 0, 3, 3, 7, 4,
+        4, 0, 3,  3, 7, 4,
         // Right face
-        1, 5, 6, 6, 2, 1,
+        1, 5, 6,  6, 2, 1,
         // Top face
-        3, 2, 6, 6, 7, 3,
+        3, 2, 6,  6, 7, 3,
         // Bottom face
-        4, 5, 1, 1, 0, 4};
+        4, 5, 1,  1, 0, 4
+    };
 
     // Generate and bind VAO
     glGenVertexArrays(1, &m_vao);
